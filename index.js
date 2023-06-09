536520ef4787e21e4fb541828e46f9bc3cae9a5d
yunnooo <jgr4739@mail.ru>
2023-06-09 16:43:05 +0300

    add troops economy, add new troop type(psionics)

--- index.js
+++ index.js
@@ -5,29 +5,52 @@ const prompts = require('prompts');
 const bloodCoins = 135;
 
 const heavyMarine = {
-  type: 'heavy marine',
-  hp: 75,
-  dmg: 15,
-  armor: 10,
+  class: 'heavy marine',
+  type: 'armor',
+  hp: 160,
+  dmg: 25,
+  armor: `70%`,
   cost: 135,
 };
 
-const marine = {
+const LightMarine = {
+  class: 'light marine',
+  type: 'light',
+  hp: 170,
+  dmg: 30,
+  armor: `30%`,
   cost: 75,
 };
 
 const penetrators = {
+  class: 'penetrators',
+  type: 'piercing',
+  hp: 110,
+  dmg: 45,
+  armor: `20%`,
   cost: 115,
 };
 
+const psionics = {
+  class: 'psionics',
+  type: 'psi',
+  hp: 10,
+  dmg: 19,
+  armor: `0%`,
+  cost: 240,
+}
+
 const z = (async () => {
   const heavyMarineCost = heavyMarine.cost;
-  const marineCost = marine.cost;
+  const marineCost = LightMarine.cost;
   const penetratorsCost = penetrators.cost;
+  const psionicsCost = psionics.cost;
 
   const isEnoughCoinsForHeavy = (bloodCoins >= heavyMarineCost);
   const isEnoughCoinsForMarine = (bloodCoins >= marineCost);
   const isEnoughCoinsForPenetrators = (bloodCoins >= penetratorsCost);
+  const isEnoughCoinsForPsionics = (bloodCoins >= psionicsCost);
+
   console.log(`your founds: ${bloodCoins}`);
   const buyTroops = await prompts({
     type: 'select',
@@ -41,10 +64,13 @@ const z = (async () => {
         disabled: !isEnoughCoinsForHeavy,
       },
       {
-        title: 'marine', description: `cost: ${marineCost}`, value: 'generating', disabled: !isEnoughCoinsForMarine,
+        title: 'light marine', description: `cost: ${marineCost}`, value: 'generating', disabled: !isEnoughCoinsForMarine,
       },
       {
         title: 'penetrators', description: `cost: ${penetratorsCost}`, value: 'good choise', disabled: !isEnoughCoinsForPenetrators,
+      },
+      {
+        title: 'psionics', description: `cost: ${psionicsCost}`, value: '', disabled: !isEnoughCoinsForPsionics,
       },
     ],
     initial: 0,
