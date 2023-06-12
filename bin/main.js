8623aa6a1e4bffe55ccfab1fe6ce5467169ba57a
toDoListEnjoyer <toDoListEnjoyer@gmail.com>
2023-06-12 20:32:47 +0300

    fix imports in main.js

--- bin/main.js
+++ bin/main.js
@@ -1,12 +1,10 @@
-import { save, getSave } from './../save.js';
-import game from '../game.js';
-import configs from '../configs.js';
+import game from '../bin/game.js';
+import configs from '../bin/configs.js';
 import prompts from 'prompts';
 
 const makePrompt = async () => {
   if (game.isEnded) return;
   const { value } = await prompts(configs.menu);
-  console.log(value);
   value(game);
   makePrompt();
 };
