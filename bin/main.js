e428901e109bed0cfc54dbd17e3dba9ecef3be9a
vasmart24 <vasmart24@yandex.ru>
2023-06-13 13:42:57 +0300

    move soldiers from index.js; add forest-creeps; update main game logic

--- bin/main.js
+++ bin/main.js
@@ -1,6 +1,6 @@
+import prompts from 'prompts';
 import game from '../lib/game.js';
 import configs from '../lib/configs.js';
-import prompts from 'prompts';
 
 const makePrompt = async () => {
   if (game.isEnded) return;
