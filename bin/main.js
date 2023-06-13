e60b45ab351b25568f70af28d6a2bc4d8b5759f6
toDoListEnjoyer <toDoListEnjoyer>
2023-06-13 13:47:22 +0300

    fix many things

--- bin/main.js
+++ bin/main.js
@@ -2,9 +2,14 @@ import game from '../lib/game.js';
 import configs from '../lib/configs.js';
 import prompts from 'prompts';
 
+
+
 const makePrompt = async () => {
-  if (game.isEnded) return;
-  const { value } = await prompts(configs.menu);
-  value(game);
+  console.clear();
+  if (game.data.isEnded) return;
+  const { func } = await prompts(configs.menu);
+  func(game);
   makePrompt();
 };
+
+makePrompt();
