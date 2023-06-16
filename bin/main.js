284b398446eb1df5628fc4c89a9a55a00af6dd1b
toDoListEnjoyer <toDoListEnjoyer@gmail.com>
2023-06-16 11:23:48 +0300

    change main.js, configs.js, add functions.js

--- bin/main.js
+++ bin/main.js
@@ -1,12 +1,23 @@
 import prompts from 'prompts';
 import configs from '../lib/configs.js';
+import game from '../lib/game.js';
 
-const makePrompt = async (config = configs.menu) => {
-  console.clear();
-  const res = await prompts(config);
-  console.log(res);
-  makePrompt();
+const makePrompt = async (config) => {
+  console.log(game)
+  if (game.isEnded) return;
+  //console.clear();
+  if (config.nextPrompt) {
+    // Промпт с text
+    const next = config.nextPrompt;
+    const { value } = await prompts(config.prompt);
+    config.action(value);
+    makePrompt(configs[next]);
+  } else {
+    // Промпт с select
+    if (config.action) config.action();
+    const { next } = await prompts(config.prompt);
+    makePrompt(configs[next]);
+  }
 };
 
-makePrompt();
-
+makePrompt(configs.menu);
