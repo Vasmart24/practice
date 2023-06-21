e100753709298fdee8c662cd5f330f3b7ff37eee
qhp21 <113057594+qhp21@users.noreply.github.com>
2023-06-21 15:43:11 +0300

    update Cities.js, mutants.js

---
+++ data/mutants.js
@@ -0,0 +1,12 @@
+const firstCityMutants = {
+    kirillmokkevnin: {
+     name: 'Kirill Mokevnin',
+     hp: 400,
+     dmg: '100-120',
+     armor: '50%',
+     loot: [
+       { title: 'biomass', description: 'Can be used to heal troops in the infirmary and improve troop genes.\nCan be exchanged for biomass.\nCan be sold.', value: '' },
+     ],
+   // B value будет хранится фукция которая при победе над противником отдает игроку лут в инвентарь и вызывает промт города
+   }
+ };
