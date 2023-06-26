1b4be900dbad93ffccb920255da64c9eb176a3e7
vasmart24 <vasmart24@yandex.ru>
2023-06-26 22:11:03 +0300

    fix soldiers.js

--- data/soldiers.js
+++ data/soldiers.js
@@ -1,26 +1,17 @@
 import prompts from 'prompts';
 
 class Unit {
-<<<<<<< HEAD
-  constructor(name, type, hp, maxHp, damage, armor, description, speed, count = 1, cost = null) {
-=======
   constructor(name, type, hp, maxHp, damage, armor, speed, count = 1, cost = 0, requiredLevel = 1) {
->>>>>>> refs/remotes/origin/main
     this.name = name;
     this.type = type;
     this.hp = hp;
     this.maxHp = maxHp;
     this.damage = damage;
     this.armor = armor;
-<<<<<<< HEAD
-    this.description = description;
-=======
->>>>>>> refs/remotes/origin/main
     this.speed = speed;
     this.count = count;
     this.cost = cost;
     this.requiredLevel = requiredLevel;
   }
 }
-
 export default Unit;
